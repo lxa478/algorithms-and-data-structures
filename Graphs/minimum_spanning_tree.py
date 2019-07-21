2b297ff3d8a03fa7e7637a23cedf17b291430622
Jake Myers <jake@directedgemedia.com>
2019-07-21 01:24:29 -0700

    Initial Commit

---
+++ Graphs/minimum_spanning_tree.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+
+def prim():
+	''' Minimum Spanning Tree '''
+	pass
+	
+	
+def kruskal():
+	''' Minimum Spanning Tree '''
+	pass
---
+++ README.md
@@ -0,0 +1,1 @@
+Some general Algorithms and Data Structures.
